5fe275a72866c9d2bb4edf52eafdbdeff901145d
euronion <42553970+euronion@users.noreply.github.com>
2023-06-09 12:35:16 +0200

    Map and PyPSA-EUR countries to REMIND-EU regions

--- README_REMIND.md
+++ README_REMIND.md
@@ -10,3 +10,11 @@ pip install gams --find-links /opt/gams/gams43.3_linux_x64_64_sfx/api/python/bdi
 substitute `/opt/gams/...` with correct path to GAMS directory, e.g. `dirname $(which gams)`
 
 Details see here: https://www.gams.com/latest/docs/API_PY_GETTING_STARTED.html
+
+* Setup CPLEX
+
+For now, add CPLEX bin directory to path
+
+* Install micromamba and set environment directory accordingly (see instructions by @euronion elsewhere)
+
+* regional mapping: currently stored in `config/regionmapping_21_EU11.csv` is mapping for REMIND-EU, used to map between PyPSA-EUR countries and REMIND-EU regions. Location configured via Snakefile.
--- scripts/extract_coupling_parameters.py
+++ scripts/extract_coupling_parameters.py
@@ -2,11 +2,12 @@
 # %%
 import re
 from pathlib import Path
-from gams import transfer as gt
 
+import country_converter as coco
 import numpy as np
 import pandas as pd
 import pypsa
+from gams import transfer as gt
 
 # %%
 # Only Generation technologies (PyPSA "generator" "carriers")
@@ -42,6 +43,7 @@ map_general_to_remind = {
 map_pypsaeur_to_remind_loads = {
     "AC": ["AC"],
 }
+
 
 # %%
 def check_for_missing_carriers(n):
@@ -91,7 +93,8 @@ if "snakemake" not in globals():
             "../results/no_scenario/networks/elec_s_4_y2110_i1_ec_lcopt_1H.nc",
             "../results/no_scenario/networks/elec_s_4_y2130_i1_ec_lcopt_1H.nc",
             "../results/no_scenario/networks/elec_s_4_y2150_i1_ec_lcopt_1H.nc",
-        ]
+        ],
+        "region_mapping": "../config/regionmapping_21_EU11.csv",
     }
     snakemake.output = {
         "capacity_factors": "../results/no_scenario/coupling-parameters/i1/capacity_factors.csv",
@@ -101,6 +104,17 @@ if "snakemake" not in globals():
         "electricity_prices": "../results/no_scenario/coupling-parameters/i1/electricity_prices.csv",
         "gdx": "../results/no_scenario/coupling-parameters/i1/coupling-parameters.gdx",
     }
+
+# %%
+# Create region mapping by loading the original mapping from REMIND-EU from file
+# and then mapping ISO 3166-1 alpha-3 country codes to PyPSA-EUR ISO 3166-1 alpha-2 country codes
+region_mapping = pd.read_csv(snakemake.input["region_mapping"], sep=";").rename(
+    columns={"RegionCode": "REMIND-EU"}
+)
+region_mapping["PyPSA-EUR"] = coco.convert(
+    names=region_mapping["CountryCode"], to="ISO2"
+)
+region_mapping = region_mapping[["PyPSA-EUR", "REMIND-EU"]].set_index("PyPSA-EUR")
 
 # %%
 capacity_factors = []
@@ -117,25 +131,31 @@ for fp in snakemake.input["networks"]:
 
     # Load network
     network = pypsa.Network(fp)
-    
-    # Add information for aggregation later: country name and general technology
-    network.generators["country"] = network.generators["bus"].map(network.buses["country"])
+
+    # First map the PyPSA-EUR countries to REMIND-EU regions;
+    # .statistics(..) can then automatically take care of the aggregation
+    network.buses["region"] = network.buses["country"].map(region_mapping["REMIND-EU"])
+
+    # Add information for aggregation later: region name (REMIND-EU) and general technology
+    network.generators["region"] = network.generators["bus"].map(
+        network.buses["region"]
+    )
     network.generators["general_carrier"] = network.generators["carrier"].map(
         map_pypsaeur_to_general
     )
-    network.loads["country"] = network.loads["bus"].map(network.buses["country"])
+    network.loads["region"] = network.loads["bus"].map(network.buses["region"])
     network.loads["bus_carrier"] = network.loads["bus"].map(network.buses["carrier"])
-    
+
     # Now make sure we have all carriers in the mapping
     check_for_missing_carriers(network)
-    
+
     ## Extract coupling parameters from network
-    
+
     # Calculate capacity factors; by assigning the general carrier and grouping by here, the capacity factor is automatically
     # calculated across all "carrier" technologies that map to the same "general_carrier"
-    capacity_factor = network.statistics(comps=["Generator"], groupby=["country", "general_carrier"])[
-        "Capacity Factor"
-    ]
+    capacity_factor = network.statistics(
+        comps=["Generator"], groupby=["region", "general_carrier"]
+    )["Capacity Factor"]
     capacity_factor = capacity_factor.to_frame("value").reset_index()
     capacity_factor["year"] = year
     capacity_factors.append(capacity_factor)
@@ -144,80 +164,108 @@ for fp in snakemake.input["networks"]:
     generation_share = (
         network.statistics(
             comps=["Generator"],
-            groupby=["country", "general_carrier"],
+            groupby=["region", "general_carrier"],
             aggregate_time="sum",
         )["Supply"]
         / network.statistics(
             comps=["Generator"],
-            groupby=["country", "general_carrier"],
+            groupby=["region", "general_carrier"],
             aggregate_time="sum",
         )["Supply"].sum()
     )
     generation_share = generation_share.to_frame("value").reset_index()
     generation_share["year"] = year
     generation_shares.append(generation_share)
-    
+
     # Calculate technology installed capacities
-    installed_capacity = network.statistics(comps=["Generator"], groupby=["country", "general_carrier"])["Optimal Capacity"]
+    installed_capacity = network.statistics(
+        comps=["Generator"], groupby=["region", "general_carrier"]
+    )["Optimal Capacity"]
     installed_capacity = installed_capacity.to_frame("value").reset_index()
     installed_capacity["year"] = year
     installed_capacities.append(installed_capacity)
 
     # Calculate the market values (round-about way as the intended method of the statistics module is not yet available)
     market_value = (
-        network.statistics(comps=["Generator"], groupby=["country", "general_carrier"], aggregate_time="sum")["Revenue"]
-        /
-        network.statistics(comps=["Generator"], groupby=["country", "general_carrier"], aggregate_time="sum")["Supply"]
+        network.statistics(
+            comps=["Generator"],
+            groupby=["region", "general_carrier"],
+            aggregate_time="sum",
+        )["Revenue"]
+        / network.statistics(
+            comps=["Generator"],
+            groupby=["region", "general_carrier"],
+            aggregate_time="sum",
+        )["Supply"]
     )
     market_value = market_value.to_frame("value").reset_index()
     market_value["year"] = year
     market_values.append(market_value)
-    
+
     # Calculate load-weighted electricity prices based on bus marginal prices
-    electricity_price = ( 
-    network.statistics(comps=["Load"], groupby=["country","bus_carrier"], aggregate_time="sum")["Revenue"]
-    /
-    (-1 * network.statistics(comps=["Load"], groupby=["country","bus_carrier"], aggregate_time="sum")["Withdrawal"])
+    electricity_price = network.statistics(
+        comps=["Load"], groupby=["region", "bus_carrier"], aggregate_time="sum"
+    )["Revenue"] / (
+        -1
+        * network.statistics(
+            comps=["Load"], groupby=["region", "bus_carrier"], aggregate_time="sum"
+        )["Withdrawal"]
     )
     electricity_price = electricity_price.to_frame("value").reset_index()
     electricity_price["year"] = year
     electricity_prices.append(electricity_price)
 
+
 # %%
 ## Combine DataFrames to same format
 # Helper function
 def postprocess_dataframe(df):
-    """General function to postprocess the dataframes, combines the network-specific results into one dataframe
-    removes excess columns / sets index and sorts by country + year"""
+    """
+    General function to postprocess the dataframes, combines the network-
+    specific results into one dataframe removes excess columns / sets index and
+    sorts by region + year.
+    """
     df = pd.concat(df)
-    df = df.rename(columns={"general_carrier": "carrier", "bus_carrier": "carrier"}) # different auxiliary columns have different names; rename for consistency
-   
-    df = df.set_index(["year", "country", "carrier"]).sort_index() # set and sort by index for more logical sort order
-    
+    df = df.rename(
+        columns={"general_carrier": "carrier", "bus_carrier": "carrier"}
+    )  # different auxiliary columns have different names; rename for consistency
+
+    df = df.set_index(
+        ["year", "region", "carrier"]
+    ).sort_index()  # set and sort by index for more logical sort order
+
     df = df[["value"]]
     df = df.reset_index()
-    
+
     def map_carriers_for_remind(dg):
-        """Maps the carrier names from general technologies to REMIND technologies"""
+        """
+        Maps the carrier names from general technologies to REMIND
+        technologies.
+        """
         old_carrier = dg.iloc[0]["carrier"]
-        
+
         # Mapping for generators and loads are different
         if old_carrier in map_general_to_remind.keys():
             _map = map_general_to_remind
         elif old_carrier in map_pypsaeur_to_remind_loads.keys():
             _map = map_pypsaeur_to_remind_loads
-        
+
         new_carriers = _map[old_carrier]
-        
+
         # Repeat rows for each new carrier, create new dataframe then assign the new carrier name
-        dg = pd.DataFrame(np.repeat(dg.values, len(new_carriers), axis=0), columns=dg.columns)
+        dg = pd.DataFrame(
+            np.repeat(dg.values, len(new_carriers), axis=0), columns=dg.columns
+        )
         dg["carrier"] = new_carriers
         return dg
-    
+
     # Map carriers to REMIND technologies
-    df = df.groupby(["year","country","carrier"], group_keys=False).apply(map_carriers_for_remind)
-    
+    df = df.groupby(["year", "region", "carrier"], group_keys=False).apply(
+        map_carriers_for_remind
+    )
+
     return df
+
 
 # %%
 # Real combining happens here
@@ -239,7 +287,7 @@ for fn, df in {
     "market_values": market_values,
     "electricity_prices": electricity_prices,
 }.items():
-    df.to_csv(snakemake.output[fn] , index=False)
+    df.to_csv(snakemake.output[fn], index=False)
 
 # %%
 # Export to GAMS gdx file for coupling
@@ -248,18 +296,18 @@ gdx = gt.Container()
 # Construct sets from exemplary index; luckily all data share most of the indeces
 sets = {
     "year": market_values["year"].unique(),
-    "country": market_values["country"].unique(),
+    "region": market_values["region"].unique(),
     "carrier": market_values["carrier"].unique(),
     "load_carrier": electricity_prices["load_carrier"].unique(),
 }
 
 # First add sets to the container
 s_year = gt.Set(gdx, "year", records=sets["year"], description="simulation year")
-s_country = gt.Set(
+s_region = gt.Set(
     gdx,
-    "country",
-    records=sets["country"],
-    description="country by which the values were aggregated",
+    "region",
+    records=sets["region"],
+    description="REMIND-EU region by which the values were aggregated",
 )
 s_carrier = gt.Set(
     gdx,
@@ -278,41 +326,41 @@ s_load_carrier = gt.Set(
 c = gt.Parameter(
     gdx,
     name="capacity_factor",
-    domain=[s_year, s_country, s_carrier],
+    domain=[s_year, s_region, s_carrier],
     records=capacity_factors,
-    description="Cacacity factors of technology per year and country in p.u.",
+    description="Cacacity factors of technology per year and region in p.u.",
 )
 
 g = gt.Parameter(
     gdx,
     name="generation_share",
-    domain=[s_year, s_country, s_carrier],
+    domain=[s_year, s_region, s_carrier],
     records=generation_shares,
-    description="Share of generation of technology per year and country in p.u.",
+    description="Share of generation of technology per year and region in p.u.",
 )
 
 i = gt.Parameter(
     gdx,
     name="installed_capacity",
-    domain=[s_year, s_country, s_carrier],
+    domain=[s_year, s_region, s_carrier],
     records=installed_capacities,
-    description="Installed capacity of technology per year and country in MW",
+    description="Installed capacity of technology per year and region in MW",
 )
 
 m = gt.Parameter(
     gdx,
     name="market_value",
-    domain=[s_year, s_country, s_carrier],
+    domain=[s_year, s_region, s_carrier],
     records=market_values,
-    description="Market value of technology per year and country in EUR/MWh",
+    description="Market value of technology per year and region in EUR/MWh",
 )
 
 p = gt.Parameter(
     gdx,
     name="electricity_price",
-    domain=[s_year, s_country, s_load_carrier],
+    domain=[s_year, s_region, s_load_carrier],
     records=electricity_prices,
-    description="Electricity price per year and country in EUR/MWh",
+    description="Electricity price per year and region in EUR/MWh",
 )
 
 gdx.write(snakemake.output["gdx"])
