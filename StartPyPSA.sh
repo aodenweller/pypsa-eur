5efed8e3e14cdb3235801b6deeed889ff25a5912
euronion <42553970+euronion@users.noreply.github.com>
2023-07-05 17:32:44 +0200

    Update StartPyPSA.sh

--- StartPyPSA.sh
+++ StartPyPSA.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-# Script to start PyPSA from REMIND
-# Pass iteration number and name of output directory as command line argument
-echo "Starting PyPSA in iteration $1, output folder $2"
-cd "$(dirname "$0")"
-source /home/adrianod/software/venv_pypsa-eur/bin/activate
-snakemake -call -s Snakefile_remind --profile cluster_config/ export_all_networks_remind --config iter=$1 RDIR_remind=$2 opt=solve
+# Run model
+micromamba run --name pypsa-eur snakemake --profile cluster_config/ -call -k -s Snakefile_remind results/${2}/i${1}/coupling-parameters/PyPSAEUR2REMIND.gdx --config remind_scenario=${2} iteration=${1}
